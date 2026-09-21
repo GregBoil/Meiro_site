806ab09c8dab6d5110378ca4f16235ff3a7e317b
GregoB <90229761+GregBoil@users.noreply.github.com>
2026-09-21 16:32:39 +0800

    Route admin product management

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,8 @@ import Catalogue from "./pages/Catalogue";
 import Product from "./pages/Product";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
-import Admin from "./pages/Admin";
+import Admin, { AdminDashboard } from "./pages/Admin";
+import AdminProducts from "./pages/AdminProducts";
 import "./styles.css";
 
 const Router =
@@ -17,7 +18,10 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="admin" element={<Admin />} />
+        <Route path="admin" element={<Admin />}>
+          <Route index element={<AdminDashboard />} />
+          <Route path="products" element={<AdminProducts />} />
+        </Route>
         <Route element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="catalogue" element={<Catalogue />} />
