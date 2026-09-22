ca34c89b1dfbed0552cd286ea7cca4a5bd55c8c8
GregoB <90229761+GregBoil@users.noreply.github.com>
2026-09-22 23:41:50 +0800

    Route inventory history page

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,7 @@ import Admin, { AdminDashboard } from "./pages/Admin";
 import AdminProducts from "./pages/AdminProducts";
 import AdminProductEditor from "./pages/AdminProductEditor";
 import AdminInventory from "./pages/AdminInventory";
+import AdminInventoryHistory from "./pages/AdminInventoryHistory";
 import AdminCollections from "./pages/AdminCollections";
 import AdminMedia from "./pages/AdminMedia";
 import AdminHome from "./pages/AdminHome";
@@ -28,6 +29,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
           <Route path="products" element={<AdminProducts />} />
           <Route path="products/:id" element={<AdminProductEditor />} />
           <Route path="inventory" element={<AdminInventory />} />
+          <Route path="inventory/history" element={<AdminInventoryHistory />} />
           <Route path="collections" element={<AdminCollections />} />
           <Route path="media" element={<AdminMedia />} />
           <Route path="home" element={<AdminHome />} />
